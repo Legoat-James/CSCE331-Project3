e4d804f0e45de7bf2f10aa7be6de5606d9bcd8f3
Legoat-James <cambroziak_01@tamu.edu>
2026-03-19 20:03:22 -0500

    updated the cors origin parameters

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ['https://csce-331-project3-frontend.vercel.app',
+          'http://localhost:3000'  // for local dev
+          ]
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
