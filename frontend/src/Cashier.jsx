4e663013747d3bed0a7bc4dbf2b021b0182f3442
Legoat-James <cambroziak_01@tamu.edu>
2026-03-31 20:43:22 -0500

    added styling for the modifications window

--- frontend/src/Cashier.jsx
+++ frontend/src/Cashier.jsx
@@ -3,7 +3,8 @@ import { Button, Form } from 'react-bootstrap';
 import './Cashier.css';
 import Menuitems from './components/cashier/MenuItems';
 import OrderSummary from './components/cashier/OrderSummary';
-import apiClient from './api/client_config';
+import apiClient from './api/client_config.js';
+// import apiClient from '../../api/client_config.js';
 
 export default function Cashier() {
     const [total, setTotal] = useState(0.00);
@@ -118,23 +119,19 @@ export default function Cashier() {
     }
 
     const handleCheckout = async () => {
-        //for testing purposes this does not reset the order yet
         console.log("checkout confirmed, order items:", orderItems);
         let sanatizedOrderItems = sanitizeOrderItems(orderItems);
         console.log("sanitized order items:", sanatizedOrderItems);
         try{
-            const response = await apiClient('api/orders/create', { body: sanatizedOrderItems });
+            const response = await apiClient('/api/orders/create', { body: sanatizedOrderItems });
             console.log('order submission response:', response);
             //reset order on successful submission
             setOrderItems([]);
             setTotal(0.00);
-            setCustomerName('');
 
         } catch (error) {
             console.error('error submitting order:', error);
         }
-        
-
     }
 
     const handleCancel = () => {
