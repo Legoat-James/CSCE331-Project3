313c9458b82087d9c18cc55b9580fec275a78e75
seth-t-powell <seth.t.powell@tamu.edu>
2026-04-14 15:59:35 -0500

    Fixed manager view

--- frontend/src/Manager.jsx
+++ frontend/src/Manager.jsx
@@ -717,11 +717,6 @@ const NavBar = () => {
           </div>
         </Col>
       </Row>
-      {/* Third row for inventory table*/}
-      <InventoryTable></InventoryTable>
-      <Row>
-
-      </Row>
     </div>
   ), [salesData, chartOptions, timeframe, recentOrders, isPending, error, isSuccess, generateReport, selectedReport, reportData, reportLoading, startDate, endDate, startHour, endHour, isSingleDay, salesReportMode]);
 
